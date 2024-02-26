ed3a6df33f6a7f537f5379a612e5576f08f2cda5
unknown <prince@clouddownunder.com.augit config --global user.name prince221100>
2024-02-26 11:13:40 +0530

    one

--- database/migrations/2024_02_25_080319_create_ticket_table.php
+++ database/migrations/2024_02_25_080319_create_ticket_table.php
@@ -18,16 +18,16 @@ return new class extends Migration
             $table->unsignedBigInteger('department_id');
             $table->integer('priority')->comment('1:High,2:Med,3:Low');
             $table->text('description');
-            $table->unsignedBigInteger('attachment_id');
+            // $table->unsignedBigInteger('attachment_id');
             $table->unsignedBigInteger('user_id');
             $table->integer('status')->comment('1:open,2:inprogress,3:close');
             $table->date('date');
-            $table->unsignedBigInteger('respond_id');
+            // $table->unsignedBigInteger('respond_id');
 
             $table->foreign('department_id')->references('id')->on('department')->onDelete('cascade');
-            $table->foreign('attachment_id')->references('id')->on('attachment')->onDelete('cascade');
+            // $table->foreign('attachment_id')->references('id')->on('attachment')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('respond_id')->references('id')->on('respond_data')->onDelete('cascade');
+            // $table->foreign('respond_id')->references('id')->on('respond_data')->onDelete('cascade');
 
 
 
